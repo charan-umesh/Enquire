7454b7548ef5627f9f3c87afc1e2889dd8b88ee4
Charan <charanahu605@gmail.com>
2020-03-17 16:10:23 +0530

    30

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 // $(document).ready(function(){
 //   $('.header').height($(window).height());
 // })
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
